10dc85853b5f523c8a063b989173ae94d9dfde4a
FJ-Riveros <fran.etnies@gmail.com>
2022-02-27 17:02:17 +0100

    Migrated the modular functionalities from the Child Theme and created a Plugin instead, to separate the components in a logical way

--- app/public/wp-content/themes/intranet-consulting-child/functions.php
+++ app/public/wp-content/themes/intranet-consulting-child/functions.php
@@ -1,15 +1,6 @@
 <?php
 
 define("INTRANET_FOLDER_PATH", get_stylesheet_directory());
-
-// Shortcodes/Forms
-require_once INTRANET_FOLDER_PATH . "/public/shortcode/form-login.php";
-require_once INTRANET_FOLDER_PATH . "/public/shortcode/form-register.php";
-
-//API REST routes
-require_once INTRANET_FOLDER_PATH . "/includes/API/api-registro.php";
-require_once INTRANET_FOLDER_PATH . "/includes/API/api-login.php";
-
 function intranet_assets()
 {
     $parent_style = "parent-style";
@@ -17,9 +8,3 @@ function intranet_assets()
     wp_enqueue_style($parent_style, get_template_directory_uri() . "/style.css", ["bootstrap"], "1.0", "all");
 }
 add_action("wp_enqueue_scripts", "intranet_assets");
-
-function intranet_register_main_menu()
-{
-    register_nav_menu("intranet_primary_menu", "Intranet Primary Menu");
-}
-add_action("init", "intranet_register_main_menu");
